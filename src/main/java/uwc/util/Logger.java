6833bce0f0e238a34a6a0cb6c125cea0a0b2329f
john-uwc <john.uwc@outlook.com>
2016-01-27 17:48:53 +0800

    refactoring smart logger

--- src/main/java/uwc/util/Logger.java
+++ src/main/java/uwc/util/Logger.java
@@ -5,54 +5,37 @@ import java.util.Stack;
 
 /**
  * @author steven
- * @version 1.0
+ * @version 1.1
  * <h1>due to log, the logger support a smart log mechanism that is based on workflow</h1>
  * <br>
  * <h2>example:</h2>
  * Logger.appendFilteEntry(packagename, tag);
- * Logger.toggle(workflow).i(content);
+ * Logger.toggle(workflow).eat(level, content);
  * Logger.reset(workflow);
  * Logger.removeFilteEntry(packagename);
  */
 public class Logger {
 
-    public static class Anchor extends Throwable{
-        protected StackTraceElement mSte = getStackTrace()[0];
-
-        public Anchor(String body){
-            super(body);
-        }
-
-        public Anchor(String format, Object... args){
-            this(String.format(format, args));
-        }
-
-        public String getBody(String workflow){
-            workflow = (null == workflow)? unclassified:workflow;
-            return String.format("<%s@%s>[%s:%s] %s"
-                    ,workflow, mSte.getMethodName(), mSte.getFileName(), mSte.getLineNumber(), getMessage());
-        }
-
-        public String getTag(String workflow){
-            if (null == workflow)
-                workflow = Filter.match(mSte.getClassName());
-            return workflow;
-        }
+    public enum Level{
+        fatal, error, warn, verbose, debug, info
     }
 
     public interface ServiceMediator {
-        public void f(String tag, String body);
-        public void e(String tag, String body);
-        public void w(String tag, String body);
-        public void v(String tag, String body);
-        public void d(String tag, String body);
-        public void i(String tag, String body);
+        public void invoke(Level level, String tag, String body);
+    }
+
+    public interface Anchor {
+        public void packTo(Level level, Stack<String> workflows, ServiceMediator mediator);
     }
 
     public static void setServiceMediator(ServiceMediator mediator){
         if(null == mediator)
             return;
         sServiceMediator = mediator;
+    }
+
+    public static String matchFilter(String key){
+        return Filter.match(key);
     }
 
     public static void appendFilteEntry(String key, String value){
@@ -70,7 +53,12 @@ public class Logger {
     private Logger(){
     }
 
-    private static ServiceMediator sServiceMediator = new SysoutMediator();
+    private static ServiceMediator sServiceMediator = new ServiceMediator() {
+        @Override
+        public void invoke(Level level, String tag, String body) {
+            System.out.printf("%s:%s", tag, body);
+        }
+    };
 
     private ThreadLocal<Stack<String>> mWorkflow = new ThreadLocal<Stack<String>>();
 
@@ -100,57 +88,9 @@ public class Logger {
     }
 
 
-    public Logger f(Anchor anchor){
-        sServiceMediator.f(anchor.getTag(null), anchor.getBody(null));
-        if (!mWorkflow.get().empty()){
-            sServiceMediator.f(
-                    anchor.getTag(mWorkflow.get().firstElement()), anchor.getBody(mWorkflow.get().lastElement()));
-        }
-        return Logger.this;
-    }
-
-    public Logger e(Anchor anchor){
-        sServiceMediator.e(anchor.getTag(null), anchor.getBody(null));
-        if (!mWorkflow.get().empty()){
-            sServiceMediator.e(
-                    anchor.getTag(mWorkflow.get().firstElement()), anchor.getBody(mWorkflow.get().lastElement()));
-        }
-        return Logger.this;
-    }
-
-    public Logger w(Anchor anchor){
-        sServiceMediator.w(anchor.getTag(null), anchor.getBody(null));
-        if (!mWorkflow.get().empty()){
-            sServiceMediator.w(
-                    anchor.getTag(mWorkflow.get().firstElement()), anchor.getBody(mWorkflow.get().lastElement()));
-        }
-        return Logger.this;
-    }
-
-    public Logger v(Anchor anchor){
-        sServiceMediator.v(anchor.getTag(null), anchor.getBody(null));
-        if (!mWorkflow.get().empty()){
-            sServiceMediator.v(
-                    anchor.getTag(mWorkflow.get().firstElement()), anchor.getBody(mWorkflow.get().lastElement()));
-        }
-        return Logger.this;
-    }
-
-    public Logger d(Anchor anchor){
-        sServiceMediator.d(anchor.getTag(null), anchor.getBody(null));
-        if (!mWorkflow.get().empty()){
-            sServiceMediator.d(
-                    anchor.getTag(mWorkflow.get().firstElement()), anchor.getBody(mWorkflow.get().lastElement()));
-        }
-        return Logger.this;
-    }
-
-    public Logger i(Anchor anchor){
-        sServiceMediator.i(anchor.getTag(null), anchor.getBody(null));
-        if (!mWorkflow.get().empty()){
-            sServiceMediator.i(
-                    anchor.getTag(mWorkflow.get().firstElement()), anchor.getBody(mWorkflow.get().lastElement()));
-        }
+    public Logger eat(Level level, Anchor anchor){
+        if(null != anchor)
+            anchor.packTo(level, mWorkflow.get(), sServiceMediator);
         return Logger.this;
     }
 
@@ -170,6 +110,11 @@ public class Logger {
         }
     }
 
+    /**
+     * @author steven
+     * @version 1.0
+     * <h1>due to manage map between package's name and tag/h1>
+     */
     private static class Filter{
 
         private static class Entry {
@@ -222,25 +167,25 @@ public class Logger {
             return (null == sFilterMap.get(hitKey))? unclassified : sFilterMap.get(hitKey).value;
         }
     }
-}
 
-class SysoutMediator implements Logger.ServiceMediator{
-    public void f(String tag, String body){
-        System.out.printf("%s:%s",tag,body);
-    }
-    public void e(String tag, String body){
-        System.out.printf("%s:%s",tag,body);
-    }
-    public void w(String tag, String body){
-        System.out.printf("%s:%s",tag,body);
-    }
-    public void v(String tag, String body){
-        System.out.printf("%s:%s",tag,body);
-    }
-    public void d(String tag, String body){
-        System.out.printf("%s:%s",tag,body);
-    }
-    public void i(String tag, String body){
-        System.out.printf("%s:%s",tag,body);
+
+
+    public static class SimpleAnchor implements Anchor{
+        private String mBody = SimpleAnchor.class.getName();
+        public SimpleAnchor(String body){
+            this.mBody = body;
+        }
+
+        public SimpleAnchor(String format, Object... args){
+            this(String.format(format, args));
+        }
+
+        public void packTo(Level level, Stack<String> workflows, ServiceMediator mediator){
+            mediator.invoke(level, SimpleAnchor.class.getSimpleName(), mBody);
+            if (workflows.empty()){
+                return;
+            }
+            mediator.invoke(level, workflows.firstElement(), workflows.lastElement() + " " + mBody);
+        }
     }
 }
